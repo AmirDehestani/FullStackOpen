b3be9cd151920eacc783012664da3650f5821aed
Amir Dehestani <amira.dehestani@gmail.com>
2023-06-24 14:09:37 -0400

    Do exercise 1.2

--- part1/src/App.js
+++ part1/src/App.js
@@ -7,18 +7,23 @@ const Header = (props) => {
   );
 };
 
-// Content component: where we show the name of each part and the number of exercises in that part
+// Content component: where we show the name of all parts and the number of exercises for each part with the help of Part component
 const Content = (props) => {
   return (
     <>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
+    </>
+  );
+};
+
+// Part component: where we show the name of each individual part and the number of exercises it contains
+const Part = (props) => {
+  return (
+    <>
       <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
+        {props.part} {props.exercises}
       </p>
     </>
   );
