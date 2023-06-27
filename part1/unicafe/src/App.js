1c1049c5af2c1b311c8f811484f83c5d3c8c3212
Amir Dehestani <amira.dehestani@gmail.com>
2023-06-27 17:49:48 -0400

    complete exercise 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,22 +1,40 @@
 import { useState } from 'react';
 
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+);
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  const increaseByOne = (feedback) => {
+    if (feedback === 'good') {
+      setGood(good + 1);
+    } else if (feedback === 'neutral') {
+      setNeutral(neutral + 1);
+    } else if (feedback === 'bad') {
+      setBad(bad + 1);
+    }
+    setTotal(total + 1);
+  };
 
   return (
     <>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Button handleClick={() => increaseByOne('good')} text={'good'} />
+      <Button handleClick={() => increaseByOne('neutral')} text={'neutral'} />
+      <Button handleClick={() => increaseByOne('bad')} text={'bad'} />
 
       <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>total {total}</p>
+      <p>average {(good - bad) / total}</p>
+      <p>positive {(good / total) * 100} %</p>
     </>
   );
 };
