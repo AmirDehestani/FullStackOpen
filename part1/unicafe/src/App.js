9ac70188c70bd500c82d366ccbc1a1e20ff3020e
Amir Dehestani <amira.dehestani@gmail.com>
2023-06-28 11:24:08 -0400

    Finish unicafe

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -17,6 +17,7 @@ const Statistics = (props) => {
   const bad = props.bad;
   const total = props.total;
 
+  // Show the statistics only if some feedback has been given
   return total ? (
     <>
       <table>
@@ -39,13 +40,15 @@ const Statistics = (props) => {
 };
 
 const App = () => {
-  // save clicks of each button to its own state
+  // Save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  // Keep track of the total feedbacks
   const [total, setTotal] = useState(0);
 
   const giveFeedback = (feedback) => {
+    // Determine what kind of feedback it is and increment it by 1
     if (feedback === 'good') {
       setGood(good + 1);
     } else if (feedback === 'neutral') {
@@ -53,13 +56,13 @@ const App = () => {
     } else if (feedback === 'bad') {
       setBad(bad + 1);
     }
+    // Regardless of the type of feedback, increment total feedbacks by 1
     setTotal(total + 1);
   };
 
   return (
     <>
       <h1>give feedback</h1>
-
       <Button handleClick={() => giveFeedback('good')} text={'good'} />
       <Button handleClick={() => giveFeedback('neutral')} text={'neutral'} />
       <Button handleClick={() => giveFeedback('bad')} text={'bad'} />
