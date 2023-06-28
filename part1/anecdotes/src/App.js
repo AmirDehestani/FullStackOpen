019163fa64d3ec2daa62fb761f196dbc809b918c
Amir Dehestani <amira.dehestani@gmail.com>
2023-06-28 15:48:15 -0400

    submit exercise 1.12

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,5 +1,9 @@
 import { useState } from 'react';
 
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+);
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often.',
@@ -14,7 +18,18 @@ const App = () => {
 
   const [selected, setSelected] = useState(0);
 
-  return <div>{anecdotes[selected]}</div>;
+  // A function that selects a random anecdote
+  const nextAnecdote = () => {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length);
+    setSelected(randomIndex);
+  };
+
+  return (
+    <>
+      <div>{anecdotes[selected]}</div>
+      <Button handleClick={nextAnecdote} text={'next anecdote'} />
+    </>
+  );
 };
 
 export default App;
