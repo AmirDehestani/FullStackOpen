13b4dce6648649f9198e7ddd0c3c9d16d0eb88bf
Amir Dehestani <amira.dehestani@gmail.com>
2023-07-02 22:20:59 -0400

    Submit exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,21 +1,24 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Persons from './components/Persons';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
+import axios from 'axios';
 
 const App = () => {
   // App compontent state
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newPerson, setnewPerson] = useState({ name: '', number: '' });
   const [search, setSearch] = useState('');
   const [searchResults, setSearchResults] = useState([
     { name: '', numebr: '' },
   ]);
+
+  // Load the data from database
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then((respond) => setPersons(respond.data));
+  }, []);
 
   // Handlers
   const handleNameChange = (event) => {
